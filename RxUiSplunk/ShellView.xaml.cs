6fae50129ccd474030e41c720ece29b4bbce9801
Nolan Egly <nolan.egly@headspring.com>
2018-06-27 18:10:07 -0500

    Add a shell, but don't wire it up yet

---
+++ RxUiSplunk/ShellView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+
+namespace RxUiSplunk
+{
+    // Designer chokes on generic classes
+    public abstract class ShellViewBase : ReactiveUserControl<ShellViewModel> { }
+
+    public partial class ShellView : ShellViewBase
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+
+            this
+                .WhenActivated(
+                    disposables =>
+                    {
+                        this
+                            .Bind(this.ViewModel, x => x.Greeting, x => x.Message.Text)
+                            .DisposeWith(disposables);
+                    });
+        }
+    }
+}
---
+++ RxUiSplunk/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+
+namespace RxUiSplunk
+{
+    public class ShellViewModel : ReactiveObject
+    {
+        private string _greeting;
+        public string Greeting
+        {
+            get => _greeting;
+            set => this.RaiseAndSetIfChanged(ref _greeting, value);
+        }
+    }
+}
