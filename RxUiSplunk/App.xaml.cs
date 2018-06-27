42874c06e3c246e0fd6e5e05dcfb8153ae85c3e7
Nolan Egly <nolan.egly@headspring.com>
2018-06-27 18:13:42 -0500

    Modify MainWindow to have our RxUi shell as it's content

--- RxUiSplunk/App.xaml.cs
+++ RxUiSplunk/App.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using ReactiveUI;
+using Splat;
 
 namespace RxUiSplunk
 {
@@ -17,8 +20,23 @@ namespace RxUiSplunk
         {
             base.OnStartup(e);
 
-            App.Current.MainWindow = new MainWindow();
+            DoSplatRegistrations();
+
+            var vm = Locator.Current.GetService<ShellViewModel>();
+            vm.Greeting = "Hello world!";
+
+            App.Current.MainWindow = new MainWindow(vm);
             App.Current.MainWindow.Show();
+        }
+
+        private void DoSplatRegistrations()
+        {
+            var loc = Locator.CurrentMutable;
+
+            // only registers views for use with viewmodels, viewmodels have to be registered separately
+            loc.RegisterViewsForViewModels(Assembly.GetExecutingAssembly()); 
+
+            loc.Register(() => new ShellViewModel(), typeof(ShellViewModel));
         }
     }
 }
