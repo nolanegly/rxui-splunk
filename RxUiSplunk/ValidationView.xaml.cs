e73a8f8b65606c276d137019f9465fa58dbdfe3b
Nolan Egly <nolan.egly@headspring.com>
2018-06-28 20:01:38 -0500

    Trying to observe viewmodel IsValid and set attached validation property, but it's readonly *grrrr*

--- RxUiSplunk/ValidationView.xaml.cs
+++ RxUiSplunk/ValidationView.xaml.cs
@@ -2,6 +2,9 @@
 using System.Windows.Controls;
 using ReactiveUI;
 
+using System; // This namespace is critical, to get the right override of Subscribe on ViewModel.TlaIsValid
+
+
 namespace RxUiSplunk
 {
     // Designer chokes on generic classes
@@ -24,6 +27,11 @@ namespace RxUiSplunk
                         this
                             .Bind(ViewModel, vm => vm.TlaIsValid, v => v.IsValidState.Text)
                             .DisposeWith(disposables);
+
+                        // this fails with System.InvalidOperationException: ''HasError' property was registered as read-only and cannot be modified without an authorization key.'
+                        this
+                            .WhenAnyValue(x => x.ViewModel.TlaIsValid)
+                            .Subscribe((x) => Tla.SetValue(Validation.HasErrorProperty, x));
                     });
         }
     }
